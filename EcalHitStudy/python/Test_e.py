aab308323aa3eeef35b6db1e7bcac22181ae730b
sguts <gutsx001@umn.edu>
2018-02-14 15:06:03 -0600

    First commit, struggling with event generation/geometry/global tags

---
+++ EcalHitStudy/python/Test_e.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 500
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        'file:///home/guts/CMS/CMSSW_8_0_21/src/ParticleGunStudy/StudyFileCollection/TestSamples/events_e.root'
+    )
+)
+
+process.demo = cms.EDAnalyzer('ParticleGunEcalHitStudy'
+)
+
+process.TFileService = cms.Service("TFileService",
+                                       fileName = cms.string("file:///home/guts/CMS/CMSSW_8_0_21/src/ParticleGunStudy/StudyFileCollection/TestSamples/results_e.root")
+                                   )
+
+
+process.p = cms.Path(process.demo)
